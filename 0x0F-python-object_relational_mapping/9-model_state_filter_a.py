4698657007e3ccb983ae60a5ccd3cf83e824373d
ayoub naitali <134403912+AyoubNaitaLI@users.noreply.github.com>
2024-03-23 00:08:24 +0000

    9-model_state_filter_a.py

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python3
-""" prints the first State object from the database hbtn_0e_6_usa
-"""
+""" This is the 9-model_state_filter_a module """
+
 import sys
 from model_state import Base, State
-from sqlalchemy import (create_engine)
-from sqlalchemy.orm import sessionmaker
 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.exc import NoResultFound
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
     Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
+    Session = sessionmaker()
+    Session.configure(bind=engine)
     session = Session()
-    for instance in session.query(State).filter(State.name.like('%a%')):
-        print(instance.id, instance.name, sep=": ")
+    for obj in session.query(State).order_by(State.id):
+        if 'a' in obj.name:
+            print(f"{obj.id}: {obj.name}")
