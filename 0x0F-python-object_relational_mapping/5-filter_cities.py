30e7583e1d2faea9ef0a004355805bfdfd37b928
ayoub naitali <134403912+AyoubNaitaLI@users.noreply.github.com>
2024-03-23 00:06:52 +0000

    5-filter_cities.py

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,18 +1,26 @@
 #!/usr/bin/python3
-"""  lists all states from the database hbtn_0e_0_usa """
+""" This is the 5-filter_cities module it lists all the cities
+in a state in hbtn_0e_0_usa database """
+
+
 import MySQLdb
 import sys
 
-
-if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
-    cur = db.cursor()
-    cur.execute("""SELECT cities.name FROM
-                cities INNER JOIN states ON states.id=cities.state_id
-                WHERE states.name=%s""", (sys.argv[4],))
-    rows = cur.fetchall()
-    tmp = list(row[0] for row in rows)
-    print(*tmp, sep=", ")
-    cur.close()
-    db.close()
+if __name__ == '__main__':
+    if ';' not in sys.argv[4]:
+        conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                               passwd=sys.argv[2], db=sys.argv[3])
+        cur = conn.cursor()
+        cur.execute("SELECT cities.name FROM states \
+                    INNER JOIN cities ON states.id = cities.state_id \
+                    WHERE states.name = %s \
+                    COLLATE utf8mb4_0900_as_cs \
+                    ORDER BY cities.id ASC", (sys.argv[4],))
+        query_rows = cur.fetchall()
+        if query_rows:
+            city = ", ".join(row[0] for row in query_rows)
+            print(city)
+        else:
+            print()
+        cur.close()
+        conn.close()
