f9c7bcbedd24a0433ec6699e9b7e3afbd605ae3e
ayoub naitali <134403912+AyoubNaitaLI@users.noreply.github.com>
2024-03-23 00:05:58 +0000

    3-my_safe_filter_states.py

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
-"""  lists all states from the database hbtn_0e_0_usa """
+""" This is the 3-my_safe_filter_states module it lists the states based
+on user input safely in hbtn_0e_0_usa database """
+
+
 import MySQLdb
 import sys
 
-
-if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
-    cur = db.cursor()
-    match = sys.argv[4]
-    cur.execute("SELECT * FROM states WHERE name LIKE %s", (match, ))
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
+if __name__ == '__main__':
+    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                           passwd=sys.argv[2], db=sys.argv[3])
+    cur = conn.cursor()
+    if ';' not in sys.argv[4]:
+        cur.execute("SELECT * FROM states WHERE name = %s", [sys.argv[4]])
+        query_rows = cur.fetchall()
+        for row in query_rows:
+            print(row)
     cur.close()
-    db.close()
+    conn.close()
