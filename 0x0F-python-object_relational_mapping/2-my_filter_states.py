32260fbae0c0767fcefe9eddcc34064e83c45d41
ayoub naitali <134403912+AyoubNaitaLI@users.noreply.github.com>
2024-03-23 00:05:38 +0000

    2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3
-"""  lists all states from the database hbtn_0e_0_usa """
+""" This is the 2-my_filter_states module it lists the states based
+on user input in hbtn_0e_0_usa database """
+
+
 import MySQLdb
 import sys
 
-
-if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'"
-                .format(sys.argv[4]))
-    rows = cur.fetchall()
-    for row in rows:
+if __name__ == '__main__':
+    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                           passwd=sys.argv[2], db=sys.argv[3])
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states WHERE name='{0}' \
+                COLLATE utf8mb4_0900_as_cs \
+                ORDER BY id".format(sys.argv[4]))
+    query_rows = cur.fetchall()
+    for row in query_rows:
         print(row)
     cur.close()
-    db.close()
+    conn.close()
